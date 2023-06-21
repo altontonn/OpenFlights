d42ba3da57335558bbc971636625bc6c0b305e15
Newton <altontonnalumasa@gmail.com>
2023-06-21 15:08:15 +0300

    Create two methods, index and show

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::AirlinesController < ApplicationController
+  def index
+    airlines = Airline.all
+    render json: AirlineSerializer.new(airlines).serialized_json
+  end
+
+  def show
+    airline = Airline.find_by(slug: params[:slug])
+    render json: AirlineSerializer.new(airline).serialized_json
+  end
 end
