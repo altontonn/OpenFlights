7b6b32494201a62d3f3fb6702f2884c6ac5defdc
Newton <altontonnalumasa@gmail.com>
2023-06-23 20:34:22 +0300

    Separate airline params from create method

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -9,11 +9,8 @@ class Api::V1::AirlinesController < ApplicationController
     render json: AirlineSerializer.new(airline).serialized_json
   end
 
-  def create
-
-    private
-    def airline_params
-      params.require(:airline).permit(:name, :image_url)
-    end
+  private
+  def airline_params
+    params.require(:airline).permit(:name, :image_url)
   end
 end
