f2c3f032c41ac2449bef754157fc77a477ae9868
Newton <altontonnalumasa@gmail.com>
2023-06-23 20:33:25 +0300

    Create airline params

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -8,4 +8,12 @@ class Api::V1::AirlinesController < ApplicationController
     airline = Airline.find_by(slug: params[:slug])
     render json: AirlineSerializer.new(airline).serialized_json
   end
+
+  def create
+
+    private
+    def airline_params
+      params.require(:airline).permit(:name, :image_url)
+    end
+  end
 end
