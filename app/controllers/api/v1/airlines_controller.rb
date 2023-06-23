70ff0c39fdfe777fa527cbc4712c0a3d4a867d32
Newton <altontonnalumasa@gmail.com>
2023-06-24 00:41:49 +0300

    Fix ruby linters

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -12,13 +12,14 @@ class Api::V1::AirlinesController < ApplicationController
   def create
     airline = Airline.new(airline_params)
     if airline.save
-      render json:AirlineSerializer.new(airline).serialized_json
+      render json: AirlineSerializer.new(airline).serialized_json
     else
-      render json: {error: airline.errors.messages}, status:
+      render json: { error: airline.errors.messages }, status:
     end
   end
 
   private
+
   def airline_params
     params.require(:airline).permit(:name, :image_url)
   end
