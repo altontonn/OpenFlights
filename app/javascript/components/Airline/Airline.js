4c923e78c1e66c1cd1837f66dc06d439cc98537a
Newton <altontonnalumasa@gmail.com>
2023-07-23 19:40:28 +0300

    Set airline and review as empty objects

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 const Airline = () => {
+  const {airline, setAirline} = useState([]);
+  const {review, setReview} = useState([]);
   return <div>This is the Airlines#show page for our app.</div>;
 };
 export default Airline;
