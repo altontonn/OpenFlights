f55d492b0e0aabdc277b4e6d50070e7fd5275961
Newton <altontonnalumasa@gmail.com>
2023-07-23 16:26:14 +0300

    Add style to LinkWrapper

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -25,17 +25,34 @@ const Grid = styled.div`
   padding: 20px;
 `;
 const Card = styled.div`
-border: 1px solid #efefef;
+  border: 1px solid #efefef;
   background: #fff;
   font-family: sans-serif;
-`
+`;
 const AirlineLogo = styled.div`
-height: 50px;
-`
+  height: 50px;
+`;
 const AirlineName = styled.div`
-padding:20px;
-`
-const LinkWrapper = styled.div``
+  padding: 20px;
+`;
+const LinkWrapper = styled.div`
+margin: 20px 0;
+
+a {
+  color: #fff;
+  background-color: #71b406;
+  border-radius: 4px;
+  padding: 10px 30px;
+  cursor: pointer;
+  border-radius: 3px;
+  border: 1px solid #71b406;
+  text-align: center;
+  line-height: 20px;
+  min-height: 40px;
+  margin: 7px;
+  font-weight: 600;
+  t
+`;
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([]);
