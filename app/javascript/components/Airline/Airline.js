74b498a1c4cb1d0912a70721322156877ebc4a9d
Newton <altontonnalumasa@gmail.com>
2023-07-24 14:56:10 +0300

    Fetch data from api import useparams

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,22 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 const Airline = () => {
   const {airline, setAirline} = useState([]);
   const {review, setReview} = useState([]);
+  const {slug} = useParams();
+  useEffect(() => {
+    const baseUrl = `${window.location.origin}/api/v1`
+    const url = `${baseUrl}/airlines/${slug}`;
+    axios.get(url)
+    .then( (resp) => {
+      console.log(resp.data)
+      setAirline(resp.data.data)
+    })
+    .catch( error => {
+      console.log("Error fetching data:", error)
+    })
+  }, [])
   return <div>This is the Airlines#show page for our app.</div>;
 };
 export default Airline;
