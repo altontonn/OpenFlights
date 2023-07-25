3e6ddb3f7b3ed1782d4177a2da602982ade650ca
Newton <altontonnalumasa@gmail.com>
2023-07-26 00:55:09 +0300

    Mount Header components to Airline component

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,27 +1,34 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import Header from "./Header";
 const Airline = () => {
-  const { airline, setAirline } = useState([]);
-  const { review, setReview } = useState([]);
+  const [ airline, setAirline ] = useState([]);
+  const [ review, setReview ] = useState([]);
+  const [ loaded, setLoaded ] = useState(false);
   const { slug } = useParams();
   useEffect(() => {
-    const baseUrl = `${window.location.origin}/api/v1`;
-    const url = `${baseUrl}/airlines/${slug}`;
-    axios
-      .get(url)
-      .then((resp) => {
-        console.log(resp.data);
-        setAirline(resp.data.data);
-      })
-      .catch((error) => {
-        console.log("Error fetching data:", error);
-      });
+    fetchData();
   }, []);
+  const baseUrl = `${window.location.origin}/api/v1`;
+  const url = `${baseUrl}/airlines/${slug}`;
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      setAirline(response.data.data);
+      setLoaded(true);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  }
 
   return (
     <div>
       <div className="column">
+        {
+          loaded &&
+          <Header attributes={airline.attributes} />
+        }
         <div className="reviews">[reviews will go here]</div>
       </div>
       <div className="column">[new review form will go here]</div>
--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 const Header = (props) => {
-  const {name, image_url} = props.attributes;
+  const { name, image_url } = props.attributes;
   return (
     <div className="header">
-      <img src="" alt="airline-img" width="50" />
-      <h1>[airline name will go here]</h1>
+      <img src={image_url} alt={name} width="50" />
+      <h1>{name}</h1>
     </div>
   );
 };
