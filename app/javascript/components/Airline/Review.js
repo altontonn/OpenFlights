349aff6c6dedc5c4fe4d8f3b19f39b9443fa2bd7
Newton <altontonnalumasa@gmail.com>
2023-07-30 21:50:46 +0300

    Create two div for description and rating

--- app/javascript/components/Airline/Review.js
+++ app/javascript/components/Airline/Review.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { styled } from "styled-components";
+const Card = styled.div``;
+const Title = styled.div``;
+const Description = styled.div``;
 const Review = (props) => {
-  return(
-    <div className="review-card"></div>
-  )
-}
+  return (
+    <div className="review-card">
+      <div className="review-title">title will go here</div>
+      <div className="review-description">title will go here</div>
+      <div className="review-rating">rating will go here</div>
+    </div>
+  );
+};
 export default Review;
