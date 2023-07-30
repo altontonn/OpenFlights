985d8f228039848f5e859753e6543ce493e294a2
Newton <altontonnalumasa@gmail.com>
2023-07-30 21:55:32 +0300

    Replace the div variable constants

--- app/javascript/components/Airline/Review.js
+++ app/javascript/components/Airline/Review.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { styled } from "styled-components";
-const Card = styled.div``;
-const Title = styled.div``;
-const Description = styled.div``;
+const Card = styled.div`
+  border-radius: 4px;
+  border: 1px solid #e6e6e6;
+  padding: 20px;
+  margin: 0px 0px 20px 0px;
+`;
+const Title = styled.div`
+  padding: 20px 0px;
+  font-weight: bold;
+`;
+const Description = styled.div`
+  padding: 0 0 20px 0;
+`;
 const Review = (props) => {
   return (
-    <div className="review-card">
-      <div className="review-title">title will go here</div>
-      <div className="review-description">title will go here</div>
-      <div className="review-rating">rating will go here</div>
-    </div>
+    <Card>
+      <Title>{props.title}</Title>
+      <Description>{props.description}</Description>
+      <div className="review-rating">{props.score}</div>
+    </Card>
   );
 };
 export default Review;
