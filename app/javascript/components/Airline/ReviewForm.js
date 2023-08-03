82222733844b15e5d353935cabde2c6201e40c20
Newton <altontonnalumasa@gmail.com>
2023-08-03 17:08:48 +0300

    create input and label

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { styled } from "styled-components";
 const ReviewForm = (props) => {
+  const ratingOptions = [5, 4, 3, 2, 1].map((score, index) => {
+    return (
+      <Fragment key={index}>
+        <input
+          type="radio"
+          value={score}
+          checked={props.review.score == score}
+          onChange={() => console.log("onChange")}
+          name="rating"
+          id={`rating-${score}`}
+        />
+        <label onClick={props.setRating.bind(this, score)}></label>
+      </Fragment>
+    );
+  });
   return (
     <div>
       <form onSubmit={props.handleSubmit}>
