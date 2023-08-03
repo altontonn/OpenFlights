5cd23d582b9d3e08e65e4ad50cadfd57726893a1
Newton <altontonnalumasa@gmail.com>
2023-08-03 13:55:51 +0300

    Create a css file

--- app/javascript/components/Rating/Rating.js
+++ app/javascript/components/Rating/Rating.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Rating.css';
+
 const Rating = (props) => {
   const score = (props.score / 5) * 100;
   return (
     <span className="rating-wrapper">
-      <span className="rating"></span>
+      <span className="rating" style={{ width: score + "%" }}></span>
     </span>
   )
 }
