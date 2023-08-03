1a23578a649e9c565f18e3d513116a2c54907094
Newton <altontonnalumasa@gmail.com>
2023-08-03 14:50:10 +0300

    Use generic font family keywords

--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { styled } from "styled-components";
+import Rating from "../Rating/Rating";
 
 const HeaderSection = styled.div`
   padding: 100px 100px 10px 100px;
@@ -7,11 +8,12 @@ const HeaderSection = styled.div`
   text-align: center;
 `;
 const Header = (props) => {
-  const { name, image_url } = props.attributes;
+  const { name, image_url, avg_score } = props.attributes;
   return (
     <HeaderSection>
       <img src={image_url} alt={name} width="50" />
       <h1>{name}</h1>
+      <Rating score={avg_score} />
     </HeaderSection>
   );
 };
